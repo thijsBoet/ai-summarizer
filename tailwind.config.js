f1503c73b99037d2c393fd71821b91354f29fa2d
Matthijs Boet <m.boet@chello.nl>
2023-06-26 03:28:00 +0200

    created app folder structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
 		'./src/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				satoshi: ['Satoshi', 'sans-serif'],
+				inter: ['Inter', 'sans-serif'],
+			}
+		},
 	},
 	plugins: [],
 };
