d08f8561e774b9da0640c5b45146febf5275057c
Matthijs Boet <m.boet@chello.nl>
2023-06-26 03:37:57 +0200

    finished Hero components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
-
+import { logo } from '../assets'
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
+		<header className='w-full flex justify-center items-center flex-col'>
+			<nav className='flex justify-between items-center w-full mb-10 pt-3'>
+				<img
+					src={logo}
+					alt='sumz logo'
+					className='w-28 object-contain'
+				/>
+				<button
+          type='button'
+          className='black_btn'
+					onClick={() =>
+						window.open(
+							'https://github.com/thijsBoet/ai-summarizer',
+						)
+					}>
+					Github
+				</button>
+      </nav>
+      
+      <h1 className='head_text'> 
+        Summarize Articles with <br className='max-md:hidden'/>
+        <span className='orange_gradient'>OpenAI GPT-4</span>
+      </h1>
+
+      <h2 className="desc">
+        Simplefy your reading with Summarize, an open-source article summarizer that transforms lengthy articles into clear and concise summaries.
+      </h2>
+		</header>
+  );
 }
 
 export default Hero
